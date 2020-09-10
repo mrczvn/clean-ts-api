2b5ad9bc77a0ee9209cdcf4d6b7927f1924a306a
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-10 12:46:58 -0300

    [R]: move all protocol imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
+import { EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols/email-validator'
 
 interface SutTypes {
   sut: SignUpController
