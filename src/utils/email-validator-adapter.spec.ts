2dfa955ce0dd5db8812ff1575bc4237f63742214
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-11 20:45:19 -0300

    [F]: ensure EmailValidatorAdapter.isValid return true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -4,7 +4,7 @@ describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
 
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid('invalid_emai')
 
     expect(isValid).toBe(false)
   })
