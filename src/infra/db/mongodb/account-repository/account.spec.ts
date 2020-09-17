74e044b5bbf2fe5a7e99d7b834760f734bbb3944
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-17 12:24:00 -0300

    [T]: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -16,7 +16,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
 
     await accountCollection.deleteMany({})
   })
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -14,7 +14,7 @@ export const MongoHelper = {
   async disconnect(): Promise<void> {
     await this.client.close()
 
-    this.uri = null
+    this.client = null
   },
 
   async getCollection(name: string): Promise<Collection> {
