7674734a5fa97f237bd3c56dc43d756eebeffd9d
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-17 13:49:55 -0300

    [T]: ensure LogControllerDecorator.handle returns the same result of the controller.handle

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -54,4 +54,24 @@ describe('LogController Decorator', () => {
 
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest: IHttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: { name: 'Marcos Vinícius' }
+    })
+  })
 })
