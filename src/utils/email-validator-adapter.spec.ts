c9e2d57b473981c18807d435b008577cc2004bfc
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 15:36:55 -0300

    [R]: simplify code

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,9 +3,7 @@ import { EmailValidatorAdapter } from './email-validator-adapter'
 
 jest.spyOn(validator, 'isEmail').mockReturnValue(true)
 
-const makeSut = (): EmailValidatorAdapter => {
-  return new EmailValidatorAdapter()
-}
+const makeSut = (): EmailValidatorAdapter => new EmailValidatorAdapter()
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
@@ -13,17 +11,17 @@ describe('EmailValidator Adapter', () => {
 
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
 
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValidEmail = sut.isValid('invalid_email@mail.com')
 
-    expect(isValid).toBe(false)
+    expect(isValidEmail).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
     const sut = makeSut()
 
-    const isValid = sut.isValid('valid_email@mail.com')
+    const isValidEmail = sut.isValid('valid_email@mail.com')
 
-    expect(isValid).toBe(true)
+    expect(isValidEmail).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
