6aa6346334c9e614a260b40574fe5ef439eb6731
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 15:19:58 -0300

    [R]: simplify code

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -3,16 +3,13 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
 
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
+  afterAll(async () => await MongoHelper.disconnect())
 
   beforeEach(async () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
+
     await accountCollection.deleteMany({})
   })
 
