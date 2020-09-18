899ab808bc1c0b681e0c9727d8f433427396327d
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 12:06:10 -0300

    [T]: ensure SignInController.handle returns 400 if no password is provided

--- src/presentation/controllers/signin/signin.spec.ts
+++ src/presentation/controllers/signin/signin.spec.ts
@@ -15,4 +15,14 @@ describe('SignIn Controller', () => {
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = makeSut()
+
+    const httpRequest: IHttpRequest = { body: { email: 'any_email@mail.com' } }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
 })
