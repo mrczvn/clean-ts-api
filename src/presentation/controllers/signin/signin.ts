158a74cf04679d69d73a7f849367c1c267fda244
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 12:40:35 -0300

    ensure SignInController.handle returns 500 if EmailValidator throws

--- src/presentation/controllers/signin/signin.ts
+++ src/presentation/controllers/signin/signin.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
 import { IEmailValidator } from '../signup/signup-protocols'
 
@@ -11,16 +11,20 @@ export class SignInController implements IController {
   }
 
   async handle(req: IHttpRequest): Promise<IHttpResponse> {
-    const { email } = req.body
+    try {
+      const { email } = req.body
 
-    const requiredFields = ['email', 'password']
+      const requiredFields = ['email', 'password']
 
-    for (const field of requiredFields) {
-      if (!req.body[field]) return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!req.body[field]) return badRequest(new MissingParamError(field))
+      }
+
+      const isValidEmail = this.emailValidator.isValid(email)
+
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
+    } catch (error) {
+      serverError(error)
     }
-
-    const isValidEmail = this.emailValidator.isValid(email)
-
-    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
   }
 }
