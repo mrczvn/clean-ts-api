f0cdff4a603cfbd745af2e6c62a93ca096a9abd3
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 12:21:50 -0300

    [F]: ensure SignInController.handle calls EmailValidator with correct email

--- src/presentation/controllers/signin/signin.ts
+++ src/presentation/controllers/signin/signin.ts
@@ -1,13 +1,24 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
+import { IEmailValidator } from '../signup/signup-protocols'
 
 export class SignInController implements IController {
+  private readonly emailValidator: IEmailValidator
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle(req: IHttpRequest): Promise<IHttpResponse> {
+    const { email } = req.body
+
     const requiredFields = ['email', 'password']
 
     for (const field of requiredFields) {
       if (!req.body[field]) return badRequest(new MissingParamError(field))
     }
+
+    this.emailValidator.isValid(email)
   }
 }
