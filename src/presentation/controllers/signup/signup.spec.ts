506e28d94d371678cb93a5f9c080808c5ef6b89f
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-18 15:27:43 -0300

    [R]: simplify code

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -57,24 +57,16 @@ const makeSut = (): ISutTypes => {
 
   const sut = new SignUpController(emailValidatorStub, addAccountStub)
 
-  return {
-    sut,
-    emailValidatorStub,
-    addAccountStub
-  }
+  return { sut, emailValidatorStub, addAccountStub }
 }
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
     const { sut } = makeSut()
 
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_passwor'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
+    delete httpRequest.body.name
 
     const httpResponse = await sut.handle(httpRequest)
 
@@ -84,13 +76,9 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
 
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
+    delete httpRequest.body.email
 
     const httpResponse = await sut.handle(httpRequest)
 
@@ -100,13 +88,9 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
 
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
+    delete httpRequest.body.password
 
     const httpResponse = await sut.handle(httpRequest)
 
@@ -116,13 +100,9 @@ describe('SignUp Controller', () => {
   test('Should return 400 if no passwordConfirmation is provided', async () => {
     const { sut } = makeSut()
 
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
+    delete httpRequest.body.passwordConfirmation
 
     const httpResponse = await sut.handle(httpRequest)
 
@@ -134,14 +114,10 @@ describe('SignUp Controller', () => {
   test('Should return 400 if password confirmation fails', async () => {
     const { sut } = makeSut()
 
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'invalid_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
+    httpRequest.body.passwordConfirmation = 'invalid_password'
+
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(
